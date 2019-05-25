355c20623cda47cc1ad84c2a540857bff0726890
Dinesh Don <dinesh.opatha@gmail.com>
2019-05-25 06:52:54 -0400

    add scripts to show/hide notification panel

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -1,3 +1,9 @@
-$("#side-menu-trigger").click(function () {
-    $(".side-menu").toggleClass("show");
+// Show hide side menu
+$("#side-menu-trigger").click(function() {
+  $(".side-menu").toggleClass("show");
+});
+
+// Show hide notification panel
+$(".notification-icon").click(function() {
+  $(".notification-dropdown").toggleClass("show");
 });
